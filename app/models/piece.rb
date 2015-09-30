ae2c282c3bc712b2250694bf959e565010e3444a
Joe Ferrairo <josephferrairo@gmail.com>
2015-09-29 22:11:32 -0400

    Update piece.rb

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,10 +1,10 @@
 class Piece < ActiveRecord::Base
   belongs_to :game
   belongs_to :player
-
   enum color: [:white, :black]
-end
 
-def self.types
-  %w(Pawn Rook Knight Bishop Queen King)
+  
+  def self.types
+    %w(Pawn Rook Knight Bishop Queen King)
+  end
 end
