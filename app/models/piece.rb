21d9f89ac02f38e0545b158d817a1b893544acd3
lantrinhca <lantrinh.ca@gmail.com>
2015-09-30 19:05:24 +0000

    working on is obstructed method

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,8 +3,29 @@ class Piece < ActiveRecord::Base
   belongs_to :player
   enum color: [:white, :black]
 
-  
   def self.types
     %w(Pawn Rook Knight Bishop Queen King)
   end
+
+  def is_obstructed?(to_x,to_y)
+    obstruction_array = obstructed_squares(to_x, to_y)
+
+    return false if obstruction_array.empty?
+
+    obstruction_array.each do |square|
+      # return true if we find an obstruction
+      # return true if game.obstruction(square[0], square[1])
+      return true if obstruction?(square[0], square[1])
+    end
+
+    # default to false
+    false
+  end
+
+  # this method collects an array of squares where an obstruction could occur
+  # the checks each of those squares for any obstructing piece
+  def obstructed_squares(x, y)
+    fail NotImplementedError 'Pieces must implement #obstructed_squares'
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20150922195510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "games", force: true do |t|
-    t.string   "name"
+  create_table "games", force: :cascade do |t|
+    t.string   "name",              limit: 255
     t.integer  "state"
     t.integer  "white_player_id"
     t.integer  "black_player_id"
@@ -27,12 +27,12 @@ ActiveRecord::Schema.define(version: 20150922195510) do
     t.datetime "updated_at"
   end
 
-  create_table "pieces", force: true do |t|
-    t.string   "type"
+  create_table "pieces", force: :cascade do |t|
+    t.string   "type",       limit: 255
     t.integer  "x_position"
     t.integer  "y_position"
     t.integer  "color"
-    t.string   "image"
+    t.string   "image",      limit: 255
     t.boolean  "captured"
     t.integer  "player_id"
     t.integer  "game_id"
@@ -40,20 +40,20 @@ ActiveRecord::Schema.define(version: 20150922195510) do
     t.datetime "updated_at"
   end
 
-  create_table "players", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "players", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "name",                   limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
 
   add_index "players", ["email"], name: "index_players_on_email", unique: true, using: :btree
