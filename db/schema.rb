cf2af41ff2ef9ff964dc5e87813b09f6d71b997b
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-10-03 14:52:12 -0700

    Fix bootstrap-sass error.
    Hey all,
    
    To debug this, I started up the app and saw that the error was pointing
    to line 5 in application.scss. I looked at that line and saw that it was
    the bootstrap-sass import. I then google the error with "bootstrap sass"
    and came across this.
    
    http://stackoverflow.com/questions/22392862/undefined-method-environment-for-nilnilclass-when-importing-bootstrap
    
    It looks like there was a bug in the version of sprockets we are using. So I upgraded it & everything looks good now. :D

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20150922195510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "games", force: true do |t|
+  create_table "games", force: :cascade do |t|
     t.string   "name"
     t.integer  "state"
     t.integer  "white_player_id"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20150922195510) do
     t.datetime "updated_at"
   end
 
-  create_table "pieces", force: true do |t|
+  create_table "pieces", force: :cascade do |t|
     t.string   "type"
     t.integer  "x_position"
     t.integer  "y_position"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20150922195510) do
     t.datetime "updated_at"
   end
 
-  create_table "players", force: true do |t|
+  create_table "players", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
