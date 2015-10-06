81c44ed34cac5d2ee19efeab73850177ec6d66fa
Rachael Salter <rasalter@gmail.com>
2015-10-06 15:41:51 +0000

    Add game id's

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,21 +12,22 @@ def populate_board!
         x_position: i,
         y_position: 1,
         color: :white,
+        game_id: id,
         )
     end
 
     #White Pieces:
-    Rook.create(x_position: 0, y_position: 0, color: :white)
-    Rook.create(x_position: 7, y_position: 0, color: :white)
+    Rook.create(x_position: 0, y_position: 0, color: :white, game_id: id)
+    Rook.create(x_position: 7, y_position: 0, color: :white, game_id: id)
 
-    Knight.create(x_position: 1, y_position: 0, color: :white)
-    Knight.create(x_position: 6, y_position: 0, color: :white)
+    Knight.create(x_position: 1, y_position: 0, color: :white, game_id: id)
+    Knight.create(x_position: 6, y_position: 0, color: :white, game_id: id)
 
-    Bishop.create(x_position: 2, y_position: 0, color: :white)
-    Bishop.create(x_position: 5, y_position: 0, color: :white)
+    Bishop.create(x_position: 2, y_position: 0, color: :white, game_id: id)
+    Bishop.create(x_position: 5, y_position: 0, color: :white, game_id: id)
 
-    Queen.create(x_position: 3, y_position: 0, color: :white)
-    King.create(x_position: 4, y_position: 0, color: :white)
+    Queen.create(x_position: 3, y_position: 0, color: :white, game_id: id)
+    King.create(x_position: 4, y_position: 0, color: :white, game_id: id)
 
     #Black Pawns
     (0..7).each do |i|
@@ -34,21 +35,22 @@ def populate_board!
         x_position: i,
         y_position: 6,
         color: :black,
+        game_id: id,
         )
     end
 
     #Black Pieces
-    Rook.create(x_position: 0, y_position: 7, color: :black)
-    Rook.create(x_position: 7, y_position: 7, color: :black)
+    Rook.create(x_position: 0, y_position: 7, color: :black, game_id: id)
+    Rook.create(x_position: 7, y_position: 7, color: :black, game_id: id)
 
-    Knight.create(x_position: 1, y_position: 7, color: :black)
-    Knight.create(x_position: 6, y_position: 7, color: :black)
+    Knight.create(x_position: 1, y_position: 7, color: :black, game_id: id)
+    Knight.create(x_position: 6, y_position: 7, color: :black, game_id: id)
 
-    Bishop.create(x_position: 2, y_position: 7, color: :black)
-    Bishop.create(x_position: 5, y_position: 7, color: :black)
+    Bishop.create(x_position: 2, y_position: 7, color: :black, game_id: id)
+    Bishop.create(x_position: 5, y_position: 7, color: :black, game_id: id)
 
-    Queen.create(x_position: 3, y_position: 7, color: :black)
-    King.create(x_position: 4, y_position: 7, color: :black)
+    Queen.create(x_position: 3, y_position: 7, color: :black, game_id: id)
+    King.create(x_position: 4, y_position: 7, color: :black, game_id: id)
   end
 end
 
