3abbac554a5370cb7a628db3de5f49de8dbb861a
Rachael Salter <rasalter@gmail.com>
2015-10-06 16:57:56 +0000

    run db:migrate on local machine

--- db/schema.rb
+++ db/schema.rb
@@ -41,19 +41,18 @@ ActiveRecord::Schema.define(version: 20150922195510) do
   end
 
   create_table "players", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
 
   add_index "players", ["email"], name: "index_players_on_email", unique: true, using: :btree
