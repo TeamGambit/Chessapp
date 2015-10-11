15a43f04f72abaef4aebf16ddd6a533e24d86c57
Matt Eves <mattheweves@outlook.com>
2015-10-11 08:17:00 -0600

    Adding helpful valid_move? methods.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,8 +3,19 @@ class Piece < ActiveRecord::Base
   belongs_to :player
   enum color: [:white, :black]
 
-  
+
   def self.types
     %w(Pawn Rook Knight Bishop Queen King)
   end
+
+  def position_on_board?(x,y)
+     return false if x < 0 || x > 7
+     return false if y < 0 || y > 7
+     return true
+  end
+
+  def piece_already_in_position?(x,y)
+    x == self.x_position && y == self.y_position
+  end
+
 end
