36d10585b31f804ed0b1979b838f623f81156cda
Matt Eves <mattheweves@outlook.com>
2015-10-12 21:04:56 -0600

    Valid Move - Knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,10 @@
 class Knight < Piece
+
+  def valid_move?(x, y)
+    # Knight has no concern for obstruction, only check required is valid move of spaces.
+    return true if (self.x_position - x).abs == 2 && (self.y_position - y).abs == 1
+    return true if (self.x_position - x).abs == 1 && (self.y_position - y).abs == 2
+    return false
+  end
+  
 end
