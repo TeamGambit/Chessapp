d2ca6d1734ce2cca90ac0540ab6f70ba524ffc39
Rachael Salter <rasalter@gmail.com>
2015-10-14 01:52:57 +0000

    add valid move for king

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,15 +1,11 @@
 class King < Piece
-  def self.type
-    where(type: 'king')
-  end
-
-  def valid_move?(x, y)
+    def valid_move?(x, y)
     # King can move one square at a time in any direction. Move not valid if current position - new position
     # is greater than the absolute value of 1. 
-    return false if (self.x_position - x).abs > 1 || (self.y_position - y).abs > 1
+    return false if (delta_x).abs > 1 || (delta_y).abs > 1
 
     # Check for obstructions between current position and new position
-    if diagonal_obstruction_array(x, y) || linear_obstruction_array(x, y)
+    if is_obstructed?(dest_x,dest_y)
       return false 
     else
       return true
