4572f9a80defd4ef55d4257f091d4ac047b0d7cc
Rachael Salter <rasalter@gmail.com>
2015-10-14 02:15:31 +0000

    add valid_move? for king

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,10 +1,10 @@
 class King < Piece
     def valid_move?(x, y)
-    # King can move one square at a time in any direction. Move not valid if current position - new position
+    # King can move one square at a time in any direction. Move not valid if origin position - desination position
     # is greater than the absolute value of 1. 
-    return false if (delta_x).abs > 1 || (delta_y).abs > 1
+    return false if (self.x_position - x).abs > 1 || (self.y_position - y).abs > 1
 
-    # Check for obstructions between current position and new position
+    # Check for obstructions between origin position and destination position
     if is_obstructed?(dest_x,dest_y)
       return false 
     else
