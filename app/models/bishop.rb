5043858853722b12d0e9a7bbda10d1ada34c3880
Lan Trinh <lantrinh.ca@gmail.com>
2015-10-16 10:46:58 +0000

    add valid move bishop

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,8 @@
 class Bishop < Piece
+	def valid_move?(x,y)
+		# check for diagonal move
+		return false unless (self.x_position - x).abs != (self.y_position - y).abs
+
+		is_obstructed?(dest_x,dest_y) ? false : true
+	end
 end
