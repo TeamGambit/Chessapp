4b7683a980c7b9639add54f2ee0090818bd0328d
josephferrairo <josephferrairo@gmail.com>
2015-10-15 20:59:00 -0400

    testing board array logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,33 @@
 class GamesController < ApplicationController
 
+
   def new
+    @game = Game.new
+
+
   end
 
   def create
+
   end
 
   def show
-    
+    @board =[
+      ["test", "test", "test", "test", "test", "test", "test", "test"],
+
+      ["test", "test", "test", "test", "test", "test", "test", "test"],
+
+      ["test", "test", "test", "test", "test", "test", "test", "test"],
+
+      ["test", "test", "test", "test", "test", "test", "test", "test"],
+
+      ["test", "test", "test", "test", "test", "test", "test", "test"],
+
+      ["test", "test", "test", "test", "test", "test", "test", "test"],
+
+      ["test", "test", "test", "test", "test", "test", "test", "test"],
+
+      ["test", "test", "test", "test", "test", "test", "test", "test"]
+    ]
   end
 end
