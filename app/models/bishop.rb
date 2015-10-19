e759be2eb75d6031386084ec19c0c34afc28b91e
Lan Trinh <lantrinh.ca@gmail.com>
2015-10-19 13:54:56 -0400

    added valid move bishop test

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,8 +1,8 @@
 class Bishop < Piece
 	def valid_move?(x,y)
 		# check for diagonal move
-		return false unless (self.x_position - x).abs != (self.y_position - y).abs
+		return false unless (x_position - x).abs == (y_position - y).abs
 
-		is_obstructed?(dest_x,dest_y) ? false : true
+		is_obstructed?(x,y) ? false : true
 	end
 end
