d5a18b6272f85ea6b9b41ae265b3ca0256b703df
Matt Eves <mattheweves@outlook.com>
2015-10-20 07:58:39 -0600

    Valid Move Queen

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,19 @@
 class Queen < Piece
+
+  def valid_move?(x,y)
+    #Check obstruction
+    is_obstructed?(x,y) ? false : true
+    # Can  move in horizonal and vertical directions
+        return false unless
+          #Vertical Move
+          (x = self.x_position && y != self.y_position)
+          #Horizontal Move
+          ||
+          (x != self.x_position && y = self.y_position)
+          #Can also move diagonally
+          ||
+          (x_position - x).abs == (y_position - y).abs
+        end
+  end
+
 end
