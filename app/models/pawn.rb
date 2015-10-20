c375cedbe5be500f57e757f29da0a7b95180a634
Rachael Salter <rasalter@gmail.com>
2015-10-20 01:35:47 +0000

    Write out valid move rules pawn

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,2 +1,9 @@
 class Pawn < Piece
+    #First move. Allowed to move 1 or 2 spaces.
+    
+    #All moves. Allowed to move 1 space.
+
+    #Up 1 and horizonal to capture.
+
+    #Cannot caputure vertically.
 end
