c953dc61f4d1bc936bb4db743121e53e593c9758
Rachael Salter <rasalter@gmail.com>
2015-11-03 16:06:16 -0800

    start over

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,9 +1,3 @@
 class Pawn < Piece
-    #First move. Allowed to move 1 or 2 spaces.
-    
-    #All moves. Allowed to move 1 space.
-
-    #Up 1 and horizonal to capture.
-
-    #Cannot caputure vertically.
 end
+    
