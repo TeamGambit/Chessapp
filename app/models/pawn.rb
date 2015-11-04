03e194803072595f84affae1cb9915383f23f147
Rachael Salter <rasalter@gmail.com>
2015-11-03 16:40:28 -0800

    add valid_move_pawn

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,3 +1,20 @@
 class Pawn < Piece
+  def valid_move?(x,y)
+    return false if backwards_move?(y)
+    return false if horizontal_move?(x)  
+  end
+
+  def first_move?(y)
+    (color: :white && y_position == 1) || (color: :black && y_position == 6)
+  end
+
+  def horizontal_move?(x)
+    dest_x = (x_position - x).abs
+    dest_x != 0
+  end
+
+  def backwards_move?(y)
+    color ? y_position > y : y_position < y
+  end
 end
-    
+
