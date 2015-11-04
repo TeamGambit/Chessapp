55bf62626caae9ed301f87e74060b8582b63d701
Rachael Salter <rasalter@gmail.com>
2015-11-03 17:56:24 -0800

    remove horizontal move

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,6 +1,5 @@
 class Pawn < Piece
   def valid_move?(x,y)
-    return false if backwards_move?(y)
     return false if horizontal_move?(x)  
   end
 
@@ -11,10 +10,6 @@ class Pawn < Piece
   def horizontal_move?(x)
     dest_x = (x_position - x).abs
     dest_x != 0
-  end
-
-  def backwards_move?(y)
-    color ? y_position > y : y_position < y
   end
 end
 
